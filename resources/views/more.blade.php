a4a22f5103d405f1e0714b98c0434c3f58bc4cda
WebDev <WebDev@10.0.2.15>
2018-04-17 14:13:51 +0300

    test1

---
+++ resources/views/more.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="C:\www\uristphp\public\css\style.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic" rel="stylesheet">
+    <title>online</title>
+</head>
+<body>
+<header>
+    <nav class="top-menu">
+        <a href="#" class="nav-menu">Главная</a>
+        <a href="#" class="nav-menu">Отзывы</a>
+        <a href="index.html" class="logo-head">Новости</a>
+        <a href="#" class="nav-menu">Вход</a>
+        <a href="more2.blade.php" class="nav-menu">Регистрация</a>
+    </nav>
+</header>
+
+<main class="container">
+    <div class="intro-background"></div>
+    <div class="intro">
+        
+    </div>
+    <div class="img-background"></div>
+    <div class="content-container-1">
+        <div class="info-block">
+            <h3>Настоящий <br> городок</h3>
+            <span>- № 1 -</span>
+            <p>Седона - не аттракцион для туристов, <br> там течет своя жизнь</p>
+        </div>
+        <div class="img-block"></div>
+    </div>
+    <div class="content-container-2">
+        <div class="item-content2">
+            <div class="icon-1"></div>
+            <h2>Без установки</h2>
+            <p>Работает через браузер <br> на любой платформе</p>
+        </div>
+        <div class="item-content2">
+            <div class="icon-2"></div>
+            <h2>Просто и легко</h2>
+            <p>Так, как будто вы работаете <br> с обычным сайтом</p>
+        </div>
+        <div class="item-content2">
+            <div class="icon-3"></div>
+            <h2>Всегда под рукой</h2>
+            <p>Быстрый доступ к нужным<br> Вам документам</p>
+        </div>
+    </div>
+    <div class="content-container-3">
+        <div class="img-block2"></div>
+        <div class="info-block2">
+            <h3>Там есть <br> мост Дьявола</h3>
+            <span>- № 2 -</span>
+            <p>Да, по нему можно пройти! Если конечно <br> вы осмелитесь</p>
+        </div>
+    </div>
+    <div class="content-container-4">
+        <div class="item-content4">
+            <h3>Небольшая <br> площадь</h3>
+            <span>- № 3 -</span>
+            <p>Все достопримечательности <br> нахоятся очень близко</p>
+        </div>
+        <div class="item-content4">
+            <h3>Красивая <br> дорога</h3>
+            <span>- № 4 -</span>
+            <p>Ехать в Седону из Лас-Вегаса совем <br> не скучно!</p>
+        </div>
+        <div class="item-content4">
+            <h3>Мало <br> туристов</h3>
+            <span>- № 5 -</span>
+            <p>Большинство едет в Гранд Каньон и <br> Толпится там</p>
+        </div>
+    </div>
+    <div class="container-search-hostels">
+        <div class="hotels-search">
+            <h2>Заинтересовались?</h2>
+            <p>Укажите предпологаемые даты поездки, и мы покажем вам лучшие предложения гостиниц в Седоне</p>
+            <button onClick="getElementById('win').removeAttribute('style');" class="button-search">Поиск гостиницы в седоне</button>
+            
+        </div>
+    </div>
+    
+
+</main>
+
+
+    <footer>
+        <div class="footer-container">
+        <div class="visit">
+            <h2>что-то вставлю </h2>
+        </div>
+        <div class="visit">
+            <a class="twitter">
+                <a href="#"></a>
+             </a>
+             <a class="facebook">
+                <a href="#"></a>
+             </a>
+             <a class="youtube">
+                <a href="#"></a>
+             </a>
+        </div>
+        <div class="visit">
+            <div class="logo">
+                <span>Website by</span>
+            <a href="#"></a>
+            </div>
+        </div>
+        </div>
+    </footer>
+    
+    
+    
+   
+
+</body>
+</html>
