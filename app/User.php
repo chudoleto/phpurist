f39a80286795e14c98d40c71944b59add84094cc
Alexander Sokolov <sanya_sokolov@inbox.ru>
2018-05-31 23:30:26 +0300

    Функции получения полного имени

--- app/User.php
+++ app/User.php
@@ -22,7 +22,40 @@ class User extends Model
 	
 	public function getFullName()
 	{
-	    return $this->Name . '. ' . $this->Sename . '.' . $this->Otchestvo;
+		// убираем лишние пробелы по краям (вдруг они там есть)
+		// и всё с заглавной буквы
+		$sename = ucfirst(trim($this->Sename));
+		$name = ucfirst(trim($this->Name));
+		$оtchestvo = ucfirst(trim($this->Otchestvo));
+		
+		// в PHP конкатенация делается через точку (не через плюс)
+		// можно операцию конкатенации (точку) указывать прямо в операторе присваивания (знак равно)
+		// $ret = $ret.'строка';		можно заменить на 			$ret .= 'строка';
+		
+		// будем постепенно "наращивать" возвращаемое значение
+		$ret = '';											// сначала пусто
+		$ret .= $sename;									// прибавляем фамилию
+		$ret .= ($name) ? ' '.$name : '';					// если имя задано прибавляем имя с пробелом в начале
+		$ret .= ($оtchestvo) ? ' '.$оtchestvo : '';			// аналогично с отчеством
+		$ret = tirm($ret);									// если фамилии нет, перед именем будет пробел, убираем его
+		return $ret; // ВСЁ
+	}
+	
+	public function getFullNameShort()
+	{
+		$sename = ucfirst(trim($this->Sename));
+		$name = ucfirst(trim($this->Name));
+		$оtchestvo = ucfirst(trim($this->Otchestvo));
+		
+		if (!$sename && !$оtchestvo && $name) {
+			return $name;
+		} elseif ($sename && !$оtchestvo && $name) {
+			return $sename . ' ' . substr($name, 0, 1) . '.';
+		} elseif ($sename && $оtchestvo && $name) {
+			return $sename . ' ' . substr($name, 0, 1) . '.' . ' ' . substr($оtchestvo, 0, 1) . '.';
+		} else {
+			return $this->getFullName();
+		}
 	}
 	
 	public function Role()
